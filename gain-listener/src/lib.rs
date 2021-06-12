97dfb197e3800c10315d412d664953347f0a94c0
Timo Savola <timo.savola@iki.fi>
2021-06-12 11:49:32 +0300

    MIT license

--- gain-listener/src/lib.rs
+++ gain-listener/src/lib.rs
@@ -1,5 +1,5 @@
-// Copyright (c) 2021 Timo Savola. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Copyright (c) 2021 Timo Savola.
+// Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
 
 //! Implement TLS servers.
